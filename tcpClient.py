023c18f7ce156f9a52aee45f830c331485d584f3
Hyeeun Shin <as221323@kookmin.ac.kr>
2022-05-02 09:49:40 +0900

    코드 정리 및 구현 완성

--- tcpClient.py
+++ tcpClient.py
@@ -3,16 +3,16 @@ from socket import *
 ip ="127.0.0.1"
 port = 12345 #http port number : 80
 
+
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((ip, port))
-
-print("Client) 연결되었습니다")
+print("Client) 연결됨")
 
 inputMsg = input("Request: ")
 clientSocket.send(inputMsg.encode("utf-8"))
+print("Client) 전송 완료")
 
-print("Client) request를 전송했습니다.")
 data = clientSocket.recv(1024)
-
 print(data.decode("utf-8"))
+
 clientSocket.close()
--- tcpServer.py
+++ tcpServer.py
@@ -1,27 +1,42 @@
 from socket import *
 from datetime import datetime
 
-def get(connect):
+def get(connect, file):
     global response, host
+
+    f = open(file, 'r')
+    contents = f.readlines()
+    content = ""
+    for i in contents:
+        content += i
+
     response = response.format(header=[200, 'ok'], date=datetime.now(), url=host)
     response += ("Content-Length: " + str(len(content)) + "\n\n" + content)
     connect.send(response.encode('utf-8'))
 
-def head(connect):
+def head(connect, file):
     global response, host
+
+    f = open(file, 'r')
+    contents = f.readlines()
+    content = ""
+    for i in contents:
+        content += i
+
     response = response.format(header=[200, 'ok'], date=datetime.now(), url=host)
     connect.send(response.encode('utf-8'))
 
 def put(connect, file, add):
     global response
     txt = ""
+    tag = "</body>"
 
     with open(file, "r") as f:
         lines = f.readlines()
     with open(file, "w") as f:
         for line in lines:
-            if line.strip("\n") == "</body>":
-                f.write(add)
+            if line.strip("\n") == tag:
+                f.write("\t" + add + "\n\n")
             f.write(line)
 
     with open(file, "r") as f:
@@ -37,6 +52,7 @@ def post(connect, name, add):
     global response
     txt = ""
     filename = name + ".txt"
+
     with open(filename, "w") as f:
         f.write(add)
 
@@ -51,7 +67,7 @@ def post(connect, name, add):
 
 host = "127.0.0.1"
 port = 12345
-response = "HTTP/1.1 {header[0]} {header[1]}\nDate: {date}\nHost: {url}\n" \
+response = "HTTP/1.1 {header[0]} {header[1]}\n1Date: {date}\nHost: {url}\n" \
            "Content-Type: text/html\n"
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -63,38 +79,32 @@ connectionSocket, addr = serverSocket.accept()
 
 recvMsg = connectionSocket.recv(1024)
 decRecMsg = recvMsg.decode("utf-8")
-# print("Server) receive msg: " + decRecMsg)  #test
 
 order = decRecMsg.split()
 
 try:
-    if len(order) < 3:
-        f = open(str(order[1]), 'r')
         if order[0] == "get":
-            contents = f.readlines()
-            content = ""
-            for i in contents:
-                content += i
-            get(connectionSocket)
-        if order[0] == "head":
-            head(connectionSocket)
-    if len(order) > 2:
-        if order[0] == "put":
-            put(connectionSocket, str(order[1]), order[2])
-        if order[0] == "post":
+            get(connectionSocket, order[1])
+        elif order[0] == "head":
+            head(connectionSocket, order[1])
+        elif order[0] == "put":
+            put(connectionSocket, order[1], order[2])
+        elif order[0] == "post":
             post(connectionSocket, order[1], order[2])
+        elif order[0] == "close":
+            serverSocket.close()
+            print("Server) Good Bye")
+        else:
+            response = "HTTP/1.1 999 NOT FOUND\n(입력된 명령어는 존재하지 않음)"
+            connectionSocket.send(response.encode('utf-8'))
 
-    else:
-        response = "HTTP/1.1 404 NOT FOUND"
-        connectionSocket.send(response.encode('utf-8'))
-except:
-    response = "HTTP/1.1 404 NOT FOUND"
+except IndexError:
+    response = "HTTP/1.1 404 NOT FOUND\n(입력형식이 올바르지 않음)"
     connectionSocket.send(response.encode('utf-8'))
 
+print("Server) 전송 완료")
 
 
 
-
-serverSocket.close()
 
 
